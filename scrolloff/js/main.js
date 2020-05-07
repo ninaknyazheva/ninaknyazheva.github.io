81a1288f7ae8bca865763a648fd83e31b87dfcd2
ninaknyazheva <ninaknyazhevav@gmail.com>
2020-05-07 19:47:32 +0300

    +

---
+++ scrolloff/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.button').on('click',  function() {
+    if ($('.popup').is(':visible')) {
+      $('.popup').fadeOut();
+    } else {
+       $('.popup').fadeIn();
+       $('html, body').css('overflow','hidden');
+    }
+  });
+  $('.popup').on('click', function(event) {
+    if (event.target == this){
+      $('.popup').fadeOut();
+    }
+  });
+});
